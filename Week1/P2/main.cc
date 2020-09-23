ff2ed71798623c1f2bb957c499a436de10a9b035
Ivan Goremykin <ivngrmk@gmail.com>
2020-09-24 01:54:09 +0300

    LesserString

--- Week1/P2/main.cc
+++ Week1/P2/main.cc
@@ -1,3 +1,18 @@
-int main() {
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+int main(){
+ string a[3];
+ cin >> a[0] >> a[1] >> a[2];
+ if (a[0] <= a[1] && a[0] <= a[2]) {
+ 	cout << a[0];
+ }	
+ else if (a[1] <= a[0] && a[1] <= a[2]) {
+ 	cout << a[1];
+ } else cout << a[2];
  return 0;
 }
